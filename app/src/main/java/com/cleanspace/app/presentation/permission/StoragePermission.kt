95e730554c15da352842addc0fc2be10c8f4ecdd
jagres0039 <159308171+jagres0039@users.noreply.github.com>
2026-06-21 15:44:37 +0700

    feat: add storage permission helper

---
+++ app/src/main/java/com/cleanspace/app/presentation/permission/StoragePermission.kt
@@ -0,0 +1,21 @@
+package com.cleanspace.app.presentation.permission
+
+import android.Manifest
+import android.os.Build
+import androidx.compose.runtime.Composable
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun rememberStoragePermissions() = rememberMultiplePermissionsState(
+    permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        listOf(
+            Manifest.permission.READ_MEDIA_IMAGES,
+            Manifest.permission.READ_MEDIA_VIDEO,
+            Manifest.permission.READ_MEDIA_AUDIO,
+        )
+    } else {
+        listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    },
+)
