5fd8acb069271f35c371bd6d8fc69c2a4d7e4928
jagres0039 <159308171+jagres0039@users.noreply.github.com>
2026-06-22 01:44:02 +0700

    Wire MainActivity to new CleanSpaceApp nav graph (all P0 + new feature screens)

--- app/src/main/java/com/cleanspace/app/MainActivity.kt
+++ app/src/main/java/com/cleanspace/app/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.cleanspace.app.presentation.navigation.CleanSpaceNavHost
+import com.cleanspace.app.ui.navigation.CleanSpaceApp
 import com.cleanspace.app.ui.theme.CleanSpaceTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CleanSpaceTheme {
-                CleanSpaceNavHost()
+                CleanSpaceApp()
             }
         }
     }
